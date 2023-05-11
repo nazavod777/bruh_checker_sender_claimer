0b2d7c46bb40e3c2fa8d05eb12fcd6962a4a8b19
nazavod777 <91217742+nazavod777@users.noreply.github.com>
2023-05-12 01:35:22 +0300

    Add files via upload

---
+++ settings/config.py
@@ -0,0 +1,9 @@
+config: dict = {
+    'RPC_URL': 'https://arbitrum.blockpi.network/v1/rpc/public',
+    'CLAIM_CONTRACT_ADDRESS': '0x0857832548ab9dd3724943305b1ca5d230341b90',
+    'TOKEN_CONTRACT_ADDRESS': '0xB5B5b428e4DE365F809CeD8271D202449e5c2F72',
+    'GAS_LIMIT_CLAIM': 'auto',
+    'GWEI_CLAIM': 'auto',
+    'GAS_LIMIT_SENDER': 'auto',
+    'GWEI_SENDER': 'auto'
+}
